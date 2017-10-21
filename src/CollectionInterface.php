c168d29649894c1275dfb48d5e792bddd8869903
thinklang <thinklang0917@gmail.com>
2017-10-21 14:27:47 +0800

    创建项目

---
+++ src/CollectionInterface.php
@@ -0,0 +1,39 @@
+<?php 
+/*
+ * This file is part of the Mosquitood package.
+ *
+ * (c) Mosquitood <mosquitood@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mosquitood\Component\Collection; 
+
+/**
+ * Collection Interface
+ *
+ * @author Mosquitood <mosquitood@gmail.com>
+ */
+
+interface CollectionInterface extends \ArrayAccess, \Countable, \IteratorAggregate 
+{
+    public function set($key, $value);
+
+    public function get($key, $default = null);
+
+    public function replace(array $parameters = array());
+
+    public function add(array $parameters = array());
+
+    public function all();
+
+    public function has($key);
+
+    public function remove($key);
+
+    public function clear();
+
+    public function keys();
+
+}
